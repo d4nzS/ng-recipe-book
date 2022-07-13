19b06af41bd86ef32d396ac388d80024bd869df4
d4nzS <dzianis.karym@gmail.com>
2022-07-13 13:54:17 +0300

    add navigation in header between recipes and shopping-list components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 
 @Component({
   selector: 'app-header',
@@ -6,4 +6,11 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   public collapsed = true;
+
+  @Output()
+  private featureSelected: EventEmitter<string> = new EventEmitter<string>();
+
+  public onSelect(feature: string): void {
+    this.featureSelected.emit(feature);
+  }
 }
