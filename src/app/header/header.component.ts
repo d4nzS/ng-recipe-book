d4e1520f7e7a397edf164b7187527d5b01577f1f
d4nzS <dzianis.karym@gmail.com>
2022-07-26 17:37:08 +0300

    add logic to save recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
 
+import { DataStorageService } from "../shared/data-storage.service";
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   public collapsed = true;
+
+  constructor(private dataStorageService: DataStorageService) {
+  }
+
+  public onSaveData(): void {
+    this.dataStorageService.storeRecipes()
+      .subscribe(response => console.log(response));
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService) {
+  }
+
+  public storeRecipes(): Observable<Object> {
+    const recipes = this.recipeService.getRecipes();
+
+    return this.http.put(
+      'https://ng-recipe-book-438e2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+      recipes
+    );
   }
 }
