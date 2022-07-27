c6eee8240c78fbf2342c3b5f678f19d2592086a2
d4nzS <dzianis.karym@gmail.com>
2022-07-27 13:57:19 +0300

    add auth form in auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {
-  public isLoginMode = true;
+export class AuthComponent implements OnInit {
+  public isLoginMode = true
+
+  public authForm: FormGroup;
+
+  ngOnInit(): void {
+    this.authForm = new FormGroup({
+      'email': new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      'password': new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
+  }
 
   public onSwitchMode(): void {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  public onSubmit(): void {
+    console.log(this.authForm.value);
+
+    this.authForm.reset();
   }
 }
