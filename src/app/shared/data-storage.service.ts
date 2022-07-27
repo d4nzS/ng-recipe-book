29b2ebc50a897eec14ea4f0ab5d10d52a47dbb15
d4nzS <dzianis.karym@gmail.com>
2022-07-28 01:10:42 +0300

    create interceptor ro set toket for request query params

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { exhaustMap, map, Observable, Subscription, take, tap } from "rxjs";
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {exhaustMap, map, Observable, Subscription, take, tap} from "rxjs";
 
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
-import { AuthService } from "../auth/auth.service";
+import {RecipeService} from "../recipes/recipe.service";
+import {Recipe} from "../recipes/recipe.model";
+import {AuthService} from "../auth/auth.service";
 
 @Injectable({
   providedIn: 'root'
@@ -27,25 +27,18 @@ export class DataStorageService {
   }
 
   public fetchRecipes(): Observable<Recipe[]> {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        return this.http.get<Recipe[]>(
-          'https://ng-recipe-book-438e2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-          {
-            params: new HttpParams().set('auth', user.token)
+    return this.http
+      .get<Recipe[]>('https://ng-recipe-book-438e2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+      .pipe(
+        map(recipes => recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients
+              ? recipe.ingredients
+              : []
           }
-        );
-      }),
-      map(recipes => recipes.map(recipe => {
-        return {
-          ...recipe,
-          ingredients: recipe.ingredients
-            ? recipe.ingredients
-            : []
-        }
-      })),
-      tap(recipes => this.recipeService.setRecipes(recipes))
-    );
+        })),
+        tap(recipes => this.recipeService.setRecipes(recipes))
+      );
   }
 }
