8f0e3571b63733d058ed830c9fc4f6a48aab1740
d4nzS <dzianis.karym@gmail.com>
2022-07-27 14:14:40 +0300

    create auth service with sign up method

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {
+  }
+
+  public signup(email: string, password: string): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyA9ho2tkoPtpsTvw_Xd-Ov05PR7lvQ_NWQ',
+      {
+        email,
+        password,
+        returnSecureToken: true
+      }
+    );
+  }
+}
