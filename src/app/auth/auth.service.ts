ae9f11938faed4a8fe80f76fd505d03add61189e
d4nzS <dzianis.karym@gmail.com>
2022-07-27 18:20:03 +0300

    add handling auth data from firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import { catchError, Observable, throwError } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError, Observable, Subject, tap, throwError } from "rxjs";
+
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   idToken: string;
@@ -15,6 +17,8 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
+  public user = new Subject<User>()
+
   constructor(private http: HttpClient) {
   }
 
@@ -26,7 +30,12 @@ export class AuthService {
         password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(
+      resData.email,
+      resData.localId,
+      resData.idToken,
+      +resData.expiresIn
+    )));
   }
 
   public login(email: string, password: string): Observable<AuthResponseData> {
@@ -37,7 +46,27 @@ export class AuthService {
         password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError), tap(resData => this.handleAuthentication(
+      resData.email,
+      resData.localId,
+      resData.idToken,
+      +resData.expiresIn
+    )));
+  }
+
+  private handleAuthentication(email: string,
+                               userId: string,
+                               token: string,
+                               expiresIn: number): void {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+
+    this.user.next(user);
   }
 
   private handleError(errorRes: HttpErrorResponse): Observable<never>{
