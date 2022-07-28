ac7640126bb2b757a8b99182bdb719c82f86d9f5
d4nzS <dzianis.karym@gmail.com>
2022-07-28 16:25:46 +0300

    create shopping-list module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
-
 
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
