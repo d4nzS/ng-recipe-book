a0312884e13874378b553f5ce082ad23cd154de1
d4nzS <dzianis.karym@gmail.com>
2022-07-28 17:33:21 +0300

    fix auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from "@angular/core";
 
 import { AuthComponent } from "./auth.component";
-import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AuthRoutingModule } from "./auth-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
-    CommonModule,
     ReactiveFormsModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule {
